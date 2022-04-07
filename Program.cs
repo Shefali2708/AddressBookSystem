121896745fb01d0a923f198c2b31a4bad392003b
shefali <shefalis2708@gmail.com>
2022-04-07 22:40:30 +0530

    AddingContactFromConsole

--- Program.cs
+++ Program.cs
@@ -10,10 +10,34 @@ namespace AddressBookSystem
             //creating object of addressbook
             AddressBook addressBook = new AddressBook();
             //getting Op in Constructor with paratmers
-            addressBook.addContacts("Shefali", "Sharma", "ABC", "Nagpur", "MH", 440018, 0123456789, "Yes@gmail.com");
+            takeInputAndAddToContacts(addressBook);
+            takeInputAndAddToContacts(addressBook);
             // called Print method 
             addressBook.print();
             Console.ReadLine();
         }
+        // take Input And Add To Contacts
+        public static void takeInputAndAddToContacts(AddressBook addressBook)
+        {
+            Console.WriteLine("Enter FirstName");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email id");
+            string email = Console.ReadLine();
+            addressBook.addContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Console.ReadLine();
+        }
+
     }
 }
