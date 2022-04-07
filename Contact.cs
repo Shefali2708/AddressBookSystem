f043c9ae73a1f105891d21cff4f63fe11ba294a6
shefali <shefalis2708@gmail.com>
2022-04-07 22:35:02 +0530

    CreatingContact

---
+++ Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+        /// <summary>
+        /// property of AddressBook
+        /// </summary>
+        class Contact
+        {
+            public string fistName { get; set; }
+            public string lastName { get; set; }
+            public string address { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public int zip { get; set; }
+            public long phoneNumber { get; set; }
+            public string email { get; set; }
+        }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">->->->->-> Welcome to Address Book Program <-<-<-<-<-<");
+            //creating object of addressbook
+            AddressBook addressBook = new AddressBook();
+            //getting Op in Constructor with paratmers
+            addressBook.addContacts("Shefali", "Sharma", "ABC", "Nagpur", "MH", 440018, 0123456789, "Yes@gmail.com");
+            // called Print method 
+            addressBook.print();
+            Console.ReadLine();
+        }
+    }
+}
